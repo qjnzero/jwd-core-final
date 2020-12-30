30f740ede8412dc6b1db2f463258adfa5b7e6cc6
xfase <xfaseex1@gmail.com>
2020-12-30 23:55:02 +0300

    feat: add fields, constr, getters, setters

--- src/main/java/com/epam/jwd/core_final/domain/Spaceship.java
+++ src/main/java/com/epam/jwd/core_final/domain/Spaceship.java
@@ -1,10 +1,47 @@
 package com.epam.jwd.core_final.domain;
 
+import java.util.Map;
+
 /**
  * crew {@link java.util.Map<Role, Short>}
  * flightDistance {@link Long} - total available flight distance
  * isReadyForNextMissions {@link Boolean} - true by default. Set to false, after first failed mission
  */
 public class Spaceship extends AbstractBaseEntity {
-    //todo
+
+    private Map<Role, Short> crew;
+    private Long flightDistance;
+    private Boolean isReadyForNextMissions;
+
+
+    public Spaceship(Long id, String name, Map<Role, Short> crew, Long flightDistance) {
+        super(id, name);
+        this.crew = crew;
+        this.flightDistance = flightDistance;
+        isReadyForNextMissions = true;
+    }
+
+    public Map<Role, Short> getCrew() {
+        return crew;
+    }
+
+    public void setCrew(Map<Role, Short> crew) {
+        this.crew = crew;
+    }
+
+    public Long getFlightDistance() {
+        return flightDistance;
+    }
+
+    public void setFlightDistance(Long flightDistance) {
+        this.flightDistance = flightDistance;
+    }
+
+    public Boolean getReadyForNextMissions() {
+        return isReadyForNextMissions;
+    }
+
+    public void setReadyForNextMissions(Boolean readyForNextMissions) {
+        isReadyForNextMissions = readyForNextMissions;
+    }
 }
