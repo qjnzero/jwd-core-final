919182f3d4e26e6ec73f7541b252dbe5d7e8414d
xfase <xfaseex1@gmail.com>
2020-12-30 23:51:23 +0300

    feat: add fields, constr, getters, setters

--- src/main/java/com/epam/jwd/core_final/domain/FlightMission.java
+++ src/main/java/com/epam/jwd/core_final/domain/FlightMission.java
@@ -1,5 +1,8 @@
 package com.epam.jwd.core_final.domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Expected fields:
  * <p>
@@ -12,5 +15,81 @@ package com.epam.jwd.core_final.domain;
  * missionResult {@link MissionResult}
  */
 public class FlightMission extends AbstractBaseEntity {
-    // todo
+
+    private String missionName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Long distance;
+    private Spaceship assignedSpaceShip;
+    private List<CrewMember> assignedCrew;
+    private MissionResult missionResult;
+
+    public FlightMission(Long id, String name, String missionName, LocalDate startDate,
+                         LocalDate endDate, Long distance, Spaceship assignedSpaceShip,
+                         List<CrewMember> assignedCrew, MissionResult missionResult) {
+        super(id, name);
+        this.missionName = missionName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.distance = distance;
+        this.assignedSpaceShip = assignedSpaceShip;
+        this.assignedCrew = assignedCrew;
+        this.missionResult = missionResult;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public void setMissionName(String missionName) {
+        this.missionName = missionName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Long distance) {
+        this.distance = distance;
+    }
+
+    public Spaceship getAssignedSpaceShip() {
+        return assignedSpaceShip;
+    }
+
+    public void setAssignedSpaceShip(Spaceship assignedSpaceShip) {
+        this.assignedSpaceShip = assignedSpaceShip;
+    }
+
+    public List<CrewMember> getAssignedCrew() {
+        return assignedCrew;
+    }
+
+    public void setAssignedCrew(List<CrewMember> assignedCrew) {
+        this.assignedCrew = assignedCrew;
+    }
+
+    public MissionResult getMissionResult() {
+        return missionResult;
+    }
+
+    public void setMissionResult(MissionResult missionResult) {
+        this.missionResult = missionResult;
+    }
 }
