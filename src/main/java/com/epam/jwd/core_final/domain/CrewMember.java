d44b9102b4300ba7e8a97b3721c5035309182fd5
xfase <xfaseex1@gmail.com>
2020-12-30 21:22:20 +0300

    feat: add fields, constr, getters, setters

--- src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
+++ src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
@@ -8,5 +8,39 @@ package com.epam.jwd.core_final.domain;
  * isReadyForNextMissions {@link Boolean} - true by default. Set to false, after first failed mission
  */
 public class CrewMember extends AbstractBaseEntity {
-    // todo
+
+    private Role role;
+    private Rank rank;
+    private Boolean isReadyForNextMissions;
+
+    public CrewMember(Long id, String name, Role role, Rank rank) {
+        super(id, name);
+        this.role = role;
+        this.rank = rank;
+        isReadyForNextMissions = true;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Boolean getReadyForNextMissions() {
+        return isReadyForNextMissions;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public void setReadyForNextMissions(Boolean readyForNextMissions) {
+        isReadyForNextMissions = readyForNextMissions;
+    }
 }
