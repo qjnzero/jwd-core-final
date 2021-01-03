ca2accdca4fda2c30fbc1e9877124668cebc52b8
xfase <xfaseex1@gmail.com>
2021-01-03 14:49:39 +0300

    feat: impl SpacesjipPopulator

---
+++ src/main/java/com/epam/jwd/core_final/populator/SpaceshipPopulator.java
@@ -0,0 +1,40 @@
+package com.epam.jwd.core_final.populator;
+
+import com.epam.jwd.core_final.domain.CrewMember;
+import com.epam.jwd.core_final.domain.Role;
+import com.epam.jwd.core_final.domain.Spaceship;
+import com.epam.jwd.core_final.util.SpaceshipsResourceReader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SpaceshipPopulator implements Populator<Spaceship> {
+    @Override
+    public Collection<Spaceship> populateFromResources(String filePath) {
+        List<Spaceship> spaceships = new ArrayList<>();
+        SpaceshipsResourceReader reader = new SpaceshipsResourceReader();
+        for (List<String> lines: reader.read(filePath)) {
+            spaceships.add(new Spaceship(
+                    lines.get(0),
+                    mapperFromStringToMapRoleAndShort(lines.get(2)),
+                    Long.parseLong(lines.get(1))
+            ));
+        }
+        return spaceships;
+    }
+
+    private Map<Role, Short> mapperFromStringToMapRoleAndShort(String source) {
+        Map<Role, Short> crew = new LinkedHashMap<>();
+        source = source.substring(1, source.length() - 1);
+        String[] crewPairs = source.split(" ");
+        for (String crewPair : crewPairs) {
+            String[] roleAndAmount = crewPair.split(":");
+            crew.put(Role.resolveRoleById(Integer.parseInt(roleAndAmount[0])), Short.parseShort(roleAndAmount[1]));
+        }
+        return crew;
+    }
+}
