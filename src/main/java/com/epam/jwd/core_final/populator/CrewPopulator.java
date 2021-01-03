826f2d4273b00b50fc5169b32449dcd246996721
xfase <xfaseex1@gmail.com>
2021-01-03 14:25:50 +0300

    feat: add impl CrewPopulator

---
+++ src/main/java/com/epam/jwd/core_final/populator/CrewPopulator.java
@@ -0,0 +1,26 @@
+package com.epam.jwd.core_final.populator;
+
+import com.epam.jwd.core_final.domain.CrewMember;
+import com.epam.jwd.core_final.domain.Rank;
+import com.epam.jwd.core_final.domain.Role;
+import com.epam.jwd.core_final.util.CrewResourceReader;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class CrewPopulator implements Populator<CrewMember> {
+
+    @Override
+    public Collection<CrewMember> populateFromResources(String filePath) {
+        List<CrewMember> crewMembers = new ArrayList<>();
+        CrewResourceReader reader = new CrewResourceReader();
+        for (List<String> lines : reader.read(filePath)) {
+            crewMembers.add(new CrewMember(
+                    lines.get(1),
+                    Role.valueOf(lines.get(0)),
+                    Rank.valueOf(lines.get(2))));
+        }
+        return crewMembers;
+    }
+}
