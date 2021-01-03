a95cd0febd4b54352dc53e2885b0f89f871b69dc
xfase <xfaseex1@gmail.com>
2021-01-03 14:24:28 +0300

    feat: add Populator interface

---
+++ src/main/java/com/epam/jwd/core_final/populator/Populator.java
@@ -0,0 +1,9 @@
+package com.epam.jwd.core_final.populator;
+
+import com.epam.jwd.core_final.domain.BaseEntity;
+
+import java.util.Collection;
+
+public interface Populator<T extends BaseEntity> {
+    Collection<T> populateFromResources(String filePath);
+}
