5fdceef5147b6fccf96a7aa43b3ce462ebe0676b
xfase <xfaseex1@gmail.com>
2021-01-05 16:49:18 +0300

    feat: add SpaceshipService impl

---
+++ src/main/java/com/epam/jwd/core_final/service/impl/SpaceshipServiceImpl.java
@@ -0,0 +1,57 @@
+package com.epam.jwd.core_final.service.impl;
+
+import com.epam.jwd.core_final.context.ApplicationContext;
+import com.epam.jwd.core_final.context.impl.NassaContext;
+import com.epam.jwd.core_final.criteria.Criteria;
+import com.epam.jwd.core_final.domain.Spaceship;
+import com.epam.jwd.core_final.factory.EntityFactory;
+import com.epam.jwd.core_final.factory.impl.SpaceshipFactory;
+import com.epam.jwd.core_final.service.SpaceshipService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public class SpaceshipServiceImpl implements SpaceshipService {
+
+    private static final ApplicationContext NASSA_CONTEXT = new NassaContext();
+    private final Collection<Spaceship> spaceships;
+    private final EntityFactory<Spaceship> spaceshipFactory;
+
+    public SpaceshipServiceImpl(SpaceshipFactory spaceshipFactory) {
+        spaceships = (Collection<Spaceship>) NASSA_CONTEXT.retrieveBaseEntityList(Spaceship.class);
+        this.spaceshipFactory = spaceshipFactory;
+    }
+
+    @Override
+    public List<Spaceship> findAllSpaceships() {
+        return new ArrayList<>(spaceships);
+    }
+
+    @Override
+    public List<Spaceship> findAllSpaceshipsByCriteria(Criteria<? extends Spaceship> criteria) {
+        return null;
+    }
+
+    @Override
+    public Optional<Spaceship> findSpaceshipByCriteria(Criteria<? extends Spaceship> criteria) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Spaceship updateSpaceshipDetails(Spaceship spaceship) {
+        spaceship.setFlightDistance(1234567L);
+        return spaceship;
+    }
+
+    @Override
+    public void assignSpaceshipOnMission(Spaceship spaceship) throws RuntimeException {
+        spaceship.setReadyForNextMissions(true); // redo
+    }
+
+    @Override
+    public Spaceship createSpaceship(Spaceship spaceship) throws RuntimeException {
+        return null;
+    }
+}
