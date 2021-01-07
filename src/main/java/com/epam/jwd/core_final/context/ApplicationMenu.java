4bb4945b0761d3243243399f5bbe67ac31c9e1e0
xfase <xfaseex1@gmail.com>
2021-01-07 12:26:11 +0300

    feat: add imput handler and utils

--- src/main/java/com/epam/jwd/core_final/context/ApplicationMenu.java
+++ src/main/java/com/epam/jwd/core_final/context/ApplicationMenu.java
@@ -1,8 +1,18 @@
 package com.epam.jwd.core_final.context;
 
+import com.epam.jwd.core_final.criteria.CrewMemberCriteria;
+import com.epam.jwd.core_final.criteria.FlightMissionCriteria;
+import com.epam.jwd.core_final.criteria.SpaceshipCriteria;
 import com.epam.jwd.core_final.domain.CrewMember;
+import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.Spaceship;
+import com.epam.jwd.core_final.util.ConsoleInputerUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.epam.jwd.core_final.util.ConsoleInputerUtil.readFlightMission;
+import static com.epam.jwd.core_final.util.ConsoleInputerUtil.readInt;
 import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printCollectionToConsole;
 import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printMsgToConsole;
 
@@ -10,14 +20,16 @@ import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printMsgToConsole;
 @FunctionalInterface
 public interface ApplicationMenu {
 
+    // todo: MY_COMMENT: add logger
+
     ApplicationContext getApplicationContext();
 
     default String printAvailableOptions() {
         return "Menu:\n" +
                 "1. Get crew members\n" +
                 "2. Get spaceships\n" +
-                "3. Update crew members\n" +
-                "4. Update spaceships" +
+                "3. Update crew member\n" +
+                "4. Update spaceship" +
                 "5. Create mission\n" +
                 "6. Update mission\n" +
                 "7. Write to json file information about selected mission\n" +
@@ -28,27 +40,53 @@ public interface ApplicationMenu {
 
         printMsgToConsole(this.printAvailableOptions());
 
-        switch (input) {
-            case 1:
-                printCollectionToConsole(this.getApplicationContext().retrieveBaseEntityList(CrewMember.class));
-                break;
-            case 2:
-                printCollectionToConsole(this.getApplicationContext().retrieveBaseEntityList(Spaceship.class));
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            default:
-                break;
+        try {
+            switch (input) {
+                case 1:
+                    printCollectionToConsole(this.getApplicationContext().retrieveBaseEntityList(CrewMember.class));
+                    break;
+                case 2:
+                    printCollectionToConsole(this.getApplicationContext().retrieveBaseEntityList(Spaceship.class));
+                    break;
+                case 3:
+                    List<CrewMember> crewMembers = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(CrewMember.class));
+                    printCollectionToConsole(crewMembers);
+                    int chosenCrewMember = readInt("Choose crew member to update: ");
+                    CrewMemberCriteria criteriaCrewMember = readCrewMemberCriteria("Make up a criteria to update selected crew member: ");
+                    updateCrewMember(crewMembers.get(chosenCrewMember), criteriaCrewMember);
+                    break;
+                case 4:
+                    List<Spaceship> spaceships = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(Spaceship.class));
+                    printCollectionToConsole(spaceships);
+                    int chosenSpaceship = readInt("Choose spaceship to update: ");
+                    SpaceshipCriteria criteriaSpaceship = readSpaceshipCriteria("Make up a criteria to update selected spaceship: ");
+                    updateSpaceship(spaceships.get(chosenSpaceship), criteriaSpaceship);
+                    break;
+                case 5:
+                    readFlightMission("Enter mission information: ");
+                    break;
+                case 6:
+                    List<FlightMission> flightMissions = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(FlightMission.class));
+                    printCollectionToConsole(flightMissions);
+                    int chosenFlightMission = readInt("Choose flight mission to update: ");
+                    FlightMissionCriteria criteria = readFlightMissionCriteria("Make up a criteria to update selected flight mission: ");
+                    updateFlightMission(flightMissions.get(chosenFlightMission), criteria);
+                    break;
+                case 7:
+                    printCollectionToConsole(this.getApplicationContext().retrieveBaseEntityList(FlightMission.class));
+                    int missionNumber = readInt("Enter mission number: ");
+                    JsonWriterUtil.writeToJsonFile(missionNumber);
+                    break;
+                case 8:
+                    System.exit(0);
+                    break;
+                default:
+                    printMsgToConsole("No such operation found");
+                    break;
+            }
+        } catch (Throwable t) {
+            // todo: MY_COMMENT: clean console
+            this.printAvailableOptions();
         }
 
     }
---
+++ src/main/java/com/epam/jwd/core_final/util/ConsoleInputerUtil.java
@@ -0,0 +1,60 @@
+package com.epam.jwd.core_final.util;
+
+import com.epam.jwd.core_final.domain.FlightMission;
+import com.epam.jwd.core_final.factory.impl.FlightMissionFactory;
+import com.epam.jwd.core_final.service.impl.FlightMissionServiceImpl;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+import java.util.zip.InflaterInputStream;
+
+import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printMsgToConsole;
+
+public final class ConsoleInputerUtil {
+
+    private static final Scanner SCANNER = new Scanner(new InflaterInputStream(System.in));
+
+    private ConsoleInputerUtil() {
+    }
+
+    public static int readInt(String msg) {
+        printMsgToConsole(msg);
+
+        return SCANNER.nextInt();
+    }
+
+    public static Long readLong(String msg) {
+        printMsgToConsole(msg);
+
+        return SCANNER.nextLong();
+    }
+
+    public static String readLine(String msg) {
+        printMsgToConsole(msg);
+
+        return SCANNER.nextLine();
+    }
+
+    public static LocalDate readDate(String msg) {
+        printMsgToConsole(msg);
+
+        int day = readInt("\nEnter day: ");
+        int month = readInt("\nEnter month: ");
+        int year = readInt("\nEnter year: ");
+        return LocalDate.of(year, month, day);
+    }
+
+    public static FlightMission readFlightMission(String msg) {
+        printMsgToConsole(msg);
+
+        String missionName = readLine("Enter mission name: ");
+
+        return new FlightMissionFactory().create(
+                missionName,
+                readDate("Enter start date:"),
+                readDate("Enter end date:"),
+                readLong("Enter distance: "),
+                null // todo: MY_COMMENT: redo mission result what value should be here
+        );
+    }
+}
