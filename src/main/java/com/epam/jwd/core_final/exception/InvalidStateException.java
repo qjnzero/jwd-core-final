d77e86ded935900fef6616a0a94acbe828a93156
xfase <xfaseex1@gmail.com>
2021-01-08 22:20:19 +0300

    feat: add InvalidStateException impl

--- src/main/java/com/epam/jwd/core_final/exception/InvalidStateException.java
+++ src/main/java/com/epam/jwd/core_final/exception/InvalidStateException.java
@@ -2,4 +2,8 @@ package com.epam.jwd.core_final.exception;
 
 public class InvalidStateException extends Exception {
     // todo
+
+    public InvalidStateException(String msg) {
+        super(msg);
+    }
 }
