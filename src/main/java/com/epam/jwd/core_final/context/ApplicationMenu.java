da7097c4bb934d11b8a4defe13393f48849a411b
xfase <xfaseex1@gmail.com>
2021-01-08 21:15:07 +0300

    refactor: upd ApplicationMenu

--- src/main/java/com/epam/jwd/core_final/context/ApplicationMenu.java
+++ src/main/java/com/epam/jwd/core_final/context/ApplicationMenu.java
@@ -7,37 +7,25 @@ import com.epam.jwd.core_final.criteria.SpaceshipCriteria;
 import com.epam.jwd.core_final.domain.CrewMember;
 import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.Spaceship;
-import com.epam.jwd.core_final.factory.impl.FlightMissionFactory;
+import com.epam.jwd.core_final.service.impl.DefaultCrewService;
 import com.epam.jwd.core_final.service.impl.DefaultMissionService;
-import com.epam.jwd.core_final.util.ConsolePrinterUtil;
-import com.epam.jwd.core_final.util.ConsoleReaderUtil;
+import com.epam.jwd.core_final.service.impl.DefaultSpaceshipService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-import static com.epam.jwd.core_final.util.ConsoleReaderUtil.readCrewMemberCriteria;
-import static com.epam.jwd.core_final.util.ConsoleReaderUtil.readFlightMission;
-import static com.epam.jwd.core_final.util.ConsoleReaderUtil.readFlightMissionCriteria;
-import static com.epam.jwd.core_final.util.ConsoleReaderUtil.readInt;
-import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printCollectionToConsole;
-import static com.epam.jwd.core_final.util.ConsolePrinterUtil.printMsgToConsole;
-import static com.epam.jwd.core_final.util.ConsoleReaderUtil.readSpaceshipCriteria;
 import static com.epam.jwd.core_final.util.JsonWriterUtil.writeToJsonFile;
-import static com.epam.jwd.core_final.util.UpdaterUtil.updateCrewMember;
-import static com.epam.jwd.core_final.util.UpdaterUtil.updateFlightMission;
-import static com.epam.jwd.core_final.util.UpdaterUtil.updateSpaceship;
 
 // todo replace Object with your own types
 @FunctionalInterface
 public interface ApplicationMenu {
 
+    Scanner SCANNER = new Scanner(System.in);
+
     // todo: MY_COMMENT: add logger
 
     ApplicationContext getApplicationContext();
-
-    default void createFlightMission(FlightMission flightMission) {
-        new DefaultMissionService(new FlightMissionFactory()).createMission(flightMission);
-    }
 
     default String printAvailableOptions() {
         return "Menu:\n" +
@@ -53,56 +41,70 @@ public interface ApplicationMenu {
 
     default void handleUserInput(int input) {
 
-        printMsgToConsole(this.printAvailableOptions());
-        List<CrewMember> crewMembers = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(CrewMember.class));
-        List<Spaceship> spaceships = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(Spaceship.class));
-        List<FlightMission> flightMissions = new ArrayList(this.getApplicationContext().retrieveBaseEntityList(FlightMission.class));
+        System.out.println(this.printAvailableOptions());
+
+        List<CrewMember> crewMembers = new ArrayList<>(this.getApplicationContext().retrieveBaseEntityList(CrewMember.class));
+        List<Spaceship> spaceships = new ArrayList<>(this.getApplicationContext().retrieveBaseEntityList(Spaceship.class));
+        List<FlightMission> flightMissions = new ArrayList<>(this.getApplicationContext().retrieveBaseEntityList(FlightMission.class));
         try {
             switch (input) {
                 case 1:
-                    printCollectionToConsole(crewMembers); // !!!!without diamond brackets!!!!
+                    System.out.println("{");
+                    crewMembers.forEach(System.out::println);
+                    System.out.println("}");
                     break;
                 case 2:
-                    printCollectionToConsole(spaceships);
+                    System.out.println("{");
+                    spaceships.forEach(System.out::println);
+                    System.out.println("}");
                     break;
                 case 3:
-                    printCollectionToConsole(crewMembers);
-                    CrewMemberCriteria criteriaCrewMember = readCrewMemberCriteria("Make up a criteria to update crew member: ");
-                    updateCrewMember(criteriaCrewMember);
+                    System.out.println("{");
+                    crewMembers.forEach(System.out::println);
+                    System.out.println("}");
+                    System.out.println("Make up a criteria to update crew member: ");
+                    System.out.println("Enter a name to find crew member: ");
+                    String name = SCANNER.nextLine();
+                    CrewMemberCriteria criteriaCrewMember = new CrewMemberCriteria.Builder().withName(name).build();
+                    DefaultCrewService.INSTANCE.updateCrewMemberDetails(
+                            DefaultCrewService.INSTANCE.findCrewMemberByCriteria(criteriaCrewMember).get());
                     break;
                 case 4:
-
-                    printCollectionToConsole(spaceships);
-                    SpaceshipCriteria criteriaSpaceship = readSpaceshipCriteria("Make up a criteria to update spaceship: ");
-                    updateSpaceship(criteriaSpaceship);
+                    System.out.println("{");
+                    spaceships.forEach(System.out::println);
+                    System.out.println("}");
+                    System.out.println("Make up a criteria to update spaceship: ");
+                    System.out.println("Enter a name to find spaceship: ");
+                    String nameS = SCANNER.nextLine();
+                    SpaceshipCriteria spaceshipCriteria = new SpaceshipCriteria.Builder().withName(nameS).build();
+                    DefaultSpaceshipService.INSTANCE.updateSpaceshipDetails(
+                            DefaultSpaceshipService.INSTANCE.findSpaceshipByCriteria(spaceshipCriteria).get());
                     break;
                 case 5:
-                    FlightMission flightMission = readFlightMission("Enter mission information: ");
-                    createFlightMission(flightMission);
                     break;
                 case 6:
-
-                    printCollectionToConsole(flightMissions);
-                    FlightMissionCriteria flightMissionCriteria = readFlightMissionCriteria("Make up a criteria to update flight mission: ");
-                    updateFlightMission(flightMissionCriteria);
                     break;
                 case 7:
-                    printCollectionToConsole(flightMissions);
-                    FlightMissionCriteria flightMissionCriteriaCase7 = readFlightMissionCriteria("Make up a criteria to update flight mission: ");
-                    writeToJsonFile(flightMissionCriteriaCase7, NassaContext.OUTPUT_FILE_PATH);
+                    System.out.println("Make up a criteria to find flight mission(s): ");
+                    System.out.println("Enter flight mission name: ");
+                    String flightMissionName = SCANNER.nextLine();
+                    FlightMissionCriteria flightMissionCriteriaToPrint =
+                            new FlightMissionCriteria.Builder().withName(flightMissionName).build();
+
+                    writeToJsonFile(DefaultMissionService.INSTANCE.findAllMissionsByCriteria(flightMissionCriteriaToPrint),
+                            NassaContext.OUTPUT_FILE_PATH);
                     break;
                 case 8:
                     System.exit(0);
                     break;
                 default:
-                    printMsgToConsole("No such operation found");
+                    System.out.println("No such operation found");
                     break;
             }
         } catch (Throwable t) {
             // todo: MY_COMMENT: clean console
             this.printAvailableOptions();
         }
-
 
 
     }
