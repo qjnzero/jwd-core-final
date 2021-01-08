f830ba0b102823b6755e7ce29fca7ffa1478df67
xfase <xfaseex1@gmail.com>
2021-01-08 18:28:22 +0300

    refactor: change spaceship service

--- src/main/java/com/epam/jwd/core_final/service/impl/DefaultSpaceshipService.java
+++ src/main/java/com/epam/jwd/core_final/service/impl/DefaultSpaceshipService.java
@@ -6,7 +6,6 @@ import com.epam.jwd.core_final.criteria.Criteria;
 import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.MissionResult;
 import com.epam.jwd.core_final.domain.Spaceship;
-import com.epam.jwd.core_final.factory.EntityFactory;
 import com.epam.jwd.core_final.factory.impl.SpaceshipFactory;
 import com.epam.jwd.core_final.service.SpaceshipService;
 import sun.plugin.dom.exception.InvalidStateException;
@@ -16,14 +15,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class DefaultSpaceshipService implements SpaceshipService {
+public enum DefaultSpaceshipService implements SpaceshipService {
 
-    private static final ApplicationContext NASSA_CONTEXT = new NassaContext();
-    private final EntityFactory<Spaceship> spaceshipFactory;
+    INSTANCE;
 
-    public DefaultSpaceshipService(SpaceshipFactory spaceshipFactory) {
-        this.spaceshipFactory = spaceshipFactory;
-    }
+    private final ApplicationContext NASSA_CONTEXT = new NassaContext();
 
     @Override
     public List<Spaceship> findAllSpaceships() {
@@ -65,7 +61,14 @@ public class DefaultSpaceshipService implements SpaceshipService {
 
     @Override
     public Spaceship createSpaceship(Spaceship spaceship) throws RuntimeException {
-        Spaceship newSpaceship = spaceshipFactory.create(
+        Optional<Spaceship> duplicate = new ArrayList<>((NASSA_CONTEXT.retrieveBaseEntityList(Spaceship.class))).stream()
+                .filter(s -> s.equals(spaceship))
+                .findAny();
+        if (duplicate.isPresent()) {
+            throw new InvalidStateException("Such a spaceship has already been created.");
+        }
+
+        Spaceship newSpaceship = new SpaceshipFactory().create(
                 spaceship.getName(),
                 spaceship.getCrew(),
                 spaceship.getFlightDistance());
