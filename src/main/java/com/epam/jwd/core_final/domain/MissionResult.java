e6eb0da58f9c3581c932f06712ff2890d1a47f13
xfase <xfaseex1@gmail.com>
2021-01-08 21:27:53 +0300

    refactor: add constr and method to random easily

--- src/main/java/com/epam/jwd/core_final/domain/MissionResult.java
+++ src/main/java/com/epam/jwd/core_final/domain/MissionResult.java
@@ -1,9 +1,31 @@
 package com.epam.jwd.core_final.domain;
 
+import com.epam.jwd.core_final.exception.UnknownEntityException;
+
 public enum MissionResult {
-    CANCELLED,
-    FAILED,
-    PLANNED,
-    IN_PROGRESS,
-    COMPLETED
+    CANCELLED(1),
+    FAILED(2),
+    PLANNED(3),
+    IN_PROGRESS(4),
+    COMPLETED(5);
+
+    MissionResult(Integer id) {
+    }
+
+    public static MissionResult resolveMissionResultById(int id) {
+        if (id == 1) {
+            return MissionResult.CANCELLED;
+        } else if (id == 2) {
+            return MissionResult.FAILED;
+        } else if (id == 3) {
+            return MissionResult.PLANNED;
+        } else if (id == 4) {
+            return MissionResult.IN_PROGRESS;
+        } else if (id == 5) {
+            return MissionResult.COMPLETED;
+        } else {
+            throw new UnknownEntityException(String.valueOf(id));
+        }
+    }
+
 }
