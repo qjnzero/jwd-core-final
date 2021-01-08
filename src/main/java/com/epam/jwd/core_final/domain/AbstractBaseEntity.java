976d57b91679028144cdeaf84eb7b3131a31ac1b
xfase <xfaseex1@gmail.com>
2021-01-08 18:09:16 +0300

    feat: add equals and hashCode

--- src/main/java/com/epam/jwd/core_final/domain/AbstractBaseEntity.java
+++ src/main/java/com/epam/jwd/core_final/domain/AbstractBaseEntity.java
@@ -1,5 +1,6 @@
 package com.epam.jwd.core_final.domain;
 
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -29,5 +30,18 @@ public abstract class AbstractBaseEntity implements BaseEntity {
     public String getName() {
         // todo
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
