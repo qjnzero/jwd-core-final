8e22fff231c5fd11c843a0d66e4ef76fa7a3ad34
xfase <xfaseex1@gmail.com>
2021-01-09 08:51:35 +0300

    rafactor: change assign method

--- src/main/java/com/epam/jwd/core_final/service/impl/DefaultSpaceshipService.java
+++ src/main/java/com/epam/jwd/core_final/service/impl/DefaultSpaceshipService.java
@@ -3,6 +3,7 @@ package com.epam.jwd.core_final.service.impl;
 import com.epam.jwd.core_final.context.ApplicationContext;
 import com.epam.jwd.core_final.context.impl.NassaContext;
 import com.epam.jwd.core_final.criteria.Criteria;
+import com.epam.jwd.core_final.domain.CrewMember;
 import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.MissionResult;
 import com.epam.jwd.core_final.domain.Spaceship;
@@ -49,22 +50,11 @@ public enum DefaultSpaceshipService implements SpaceshipService {
 
     @Override
     public void assignSpaceshipOnMission(Spaceship spaceship) throws InvalidStateException {
-        if (!spaceship.getReadyForNextMissions()) {
-            throw new InvalidStateException("Spaceship isn't ready for the next flight mission.");
-        }
-
-        FlightMission flightMission = new ArrayList<>((NASSA_CONTEXT.retrieveBaseEntityList(FlightMission.class))).stream()
-                .filter(mission -> mission.getMissionResult().equals(MissionResult.PLANNED))
-                .filter(mission -> mission.getDistance() >= spaceship.getFlightDistance())
-                .findFirst()
-                .orElseThrow(() -> new InvalidStateException("No available mission found"));
-
-        flightMission.setAssignedSpaceShip(spaceship);
-
         new ArrayList<>((NASSA_CONTEXT.retrieveBaseEntityList(Spaceship.class))).stream()
-                .filter(s -> s.equals(flightMission.getAssignedSpaceShip()))
+                .filter(s -> s.equals(spaceship))
+                .filter(Spaceship::getReadyForNextMissions)
                 .findFirst()
-                .ifPresent(s -> s.setReadyForNextMissions(false));
+                .orElseThrow(() -> new InvalidStateException("No available spaceship was found to assign."));
     }
 
     @Override
