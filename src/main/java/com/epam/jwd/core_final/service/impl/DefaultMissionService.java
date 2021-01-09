c08fcd3077edfb4692b3094ce70c82408a560842
xfase <xfaseex1@gmail.com>
2021-01-09 07:48:56 +0300

    refactor: change create method

--- src/main/java/com/epam/jwd/core_final/service/impl/DefaultMissionService.java
+++ src/main/java/com/epam/jwd/core_final/service/impl/DefaultMissionService.java
@@ -6,6 +6,7 @@ import com.epam.jwd.core_final.criteria.Criteria;
 import com.epam.jwd.core_final.domain.CrewMember;
 import com.epam.jwd.core_final.domain.FlightMission;
 import com.epam.jwd.core_final.domain.MissionResult;
+import com.epam.jwd.core_final.domain.Role;
 import com.epam.jwd.core_final.domain.Spaceship;
 import com.epam.jwd.core_final.exception.InvalidStateException;
 import com.epam.jwd.core_final.factory.EntityFactory;
@@ -14,7 +15,9 @@ import com.epam.jwd.core_final.service.MissionService;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -56,8 +59,28 @@ public enum DefaultMissionService implements MissionService {
         FlightMission newFlightMission = FlightMissionFactory.INSTANCE.create(
                 flightMission.getStartDate(), flightMission.getStartDate(),
                 flightMission.getDistance(), flightMission.getMissionResult());
-        newFlightMission.setAssignedSpaceShip(findSpaceshipToAssign(newFlightMission));
-//        newFlightMission.setAssignedCrew(findCrewToAssign(newFlightMission));
+
+
+        Spaceship spaceshipToAssign = findSpaceshipToAssign(newFlightMission);
+        spaceshipToAssign.setReadyForNextMissions(false);
+        DefaultSpaceshipService.INSTANCE.findAllSpaceships().stream()
+                .filter(s -> s.equals(spaceshipToAssign))
+                .findFirst()
+                .ifPresent(s -> s.setReadyForNextMissions(false));
+        newFlightMission.setAssignedSpaceShip(spaceshipToAssign);
+
+
+        List<CrewMember> crewToAssign = findCrewToAssign(newFlightMission);
+        for (CrewMember crewMember : DefaultCrewService.INSTANCE.findAllCrewMembers()) {
+            for (CrewMember crewMemberToAssign : crewToAssign) {
+                if (crewMember.equals(crewMemberToAssign)) {
+                    crewMember.setReadyForNextMissions(false);
+                    crewMemberToAssign.setReadyForNextMissions(false);
+                }
+            }
+        }
+        newFlightMission.setAssignedCrew(crewToAssign);
+
         NassaContext.addEntityToStorage(newFlightMission, FlightMission.class);
         return newFlightMission;
     }
@@ -65,11 +88,24 @@ public enum DefaultMissionService implements MissionService {
     private Spaceship findSpaceshipToAssign(FlightMission flightMission) throws InvalidStateException {
         return new ArrayList<>(NASSA_CONTEXT.retrieveBaseEntityList(Spaceship.class)).stream()
                 .filter(spaceship -> spaceship.getFlightDistance() >= flightMission.getDistance())
+                .filter(spaceship -> spaceship.getReadyForNextMissions().equals(true))
                 .findFirst()
-                .orElseThrow(() -> new InvalidStateException());
+                .orElseThrow(() -> new InvalidStateException("No spaceship was found. Cannot assign it to mission."));
     }
 
-//    private List<CrewMember> findCrewToAssign(FlightMission flightMission) {
-//
-//    }
+    private List<CrewMember> findCrewToAssign(FlightMission flightMission) throws InvalidStateException {
+        ArrayList<CrewMember> crewMembers = new ArrayList<>(NASSA_CONTEXT.retrieveBaseEntityList(CrewMember.class));
+        Map<Role, Short> assignedCrew = flightMission.getAssignedSpaceShip().getCrew();
+        List<CrewMember> neededCrew = new ArrayList<>();
+        for (Role role : assignedCrew.keySet()) {
+            neededCrew.addAll(crewMembers.stream()
+                    .filter(crewMember -> crewMember.getRole().equals(role))
+                    .limit(assignedCrew.get(role))
+                    .collect(Collectors.toList()));
+        }
+        if (neededCrew.isEmpty()) {
+            throw new InvalidStateException("No crew was found. Cannot assign it to mission.");
+        }
+        return neededCrew;
+    }
 }
