f70be95c7cb9d8d72ea267689afd2726796f807b
xfase <xfaseex1@gmail.com>
2021-01-10 12:50:44 +0300

    refactor: reset main to defaylt

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -2,18 +2,12 @@ package com.epam.jwd.core_final;
 
 import com.epam.jwd.core_final.context.Application;
 import com.epam.jwd.core_final.exception.InvalidStateException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
 
     public static void main(String[] args) {
         try {
             Application.start();
-            LOGGER.error("TEST тест тестов123");
         } catch (InvalidStateException e) {
             e.printStackTrace();
         }
